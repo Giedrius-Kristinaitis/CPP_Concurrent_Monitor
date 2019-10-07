41a6023cf85ccdc7f473411ce4db68250e63c2e3
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-10-07 22:10:33 +0300

    Added json lib

---
+++ include/data.h
@@ -0,0 +1,30 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <string>
+
+using namespace std;
+
+class Data {
+
+    private:
+        string title;
+        double price;
+        int quantity;
+        char result;
+
+    public:
+        Data(string title, double price, int quantity);
+
+        void setTitle(string title);
+        void setPrice(double price);
+        void setQuantity(int quantity);
+        void setResult(char result);
+
+        string getTitle();
+        double getPrice();
+        int getQuantity();
+        char getResult();
+};
+
+#endif // DATA_H
---
+++ src/data.cpp
@@ -0,0 +1,42 @@
+#include "data.h"
+#include <string>
+
+using namespace std;
+
+Data::Data(string title, double price, int quantity) {
+    this->title = title;
+    this->price = price;
+    this->quantity = quantity;
+}
+
+void Data::setTitle(string title) {
+    this->title = title;
+}
+
+void Data::setPrice(double price) {
+    this->price = price;
+}
+
+void Data::setQuantity(int quantity) {
+    this->quantity = quantity;
+}
+
+void Data::setResult(char result) {
+    this->result = result;
+}
+
+string Data::getTitle() {
+    return this->title;
+}
+
+double Data::getPrice() {
+    return this->price;
+}
+
+int Data::getQuantity() {
+    return this->quantity;
+}
+
+char Data::getResult() {
+    return this->result;
+}
